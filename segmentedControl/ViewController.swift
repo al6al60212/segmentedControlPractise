2e2aeddb26083b7483e918ce7378cf63d07ac158
Heyi <al6al60212@gmail.com>
2022-08-31 20:42:58 +0800

    no if else Version

--- segmentedControl/ViewController.swift
+++ segmentedControl/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let pics = ["魔戒現身", "雙城奇謀", "王者再臨"]
+    var index = 0
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func preBtn(_ sender: Any) {
+        index = (index + pics.count - 1) % pics.count
+        imageView.image = UIImage(named: pics[index])
+        nameLable.text = pics[index]
+        pageControl.currentPage = index
+        segmentControl.selectedSegmentIndex = index
+    }
+    
+    @IBAction func nextBtn(_ sender: Any) {
+        index = (index + 1) % pics.count
+        imageView.image = UIImage(named: pics[index])
+        nameLable.text = pics[index]
+        pageControl.currentPage = index
+        segmentControl.selectedSegmentIndex = index
+    }
+    
+    
+    @IBAction func pageControlChange(_ sender: UIPageControl) {
+        index = sender.currentPage
+        imageView.image = UIImage(named: pics[index])
+        nameLable.text = pics[index]
+        segmentControl.selectedSegmentIndex = index
+    }
+    
+    @IBAction func segmentControlChange(_ sender: UISegmentedControl) {
+        index = sender.selectedSegmentIndex
+        imageView.image = UIImage(named: pics[index])
+        nameLable.text = pics[index]
+        pageControl.currentPage = index
+    }
 }
 
